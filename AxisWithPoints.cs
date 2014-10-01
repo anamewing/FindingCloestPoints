59dfc7d9e071f71ad6495e4b456ee551eaf7c197
anamewing <anamewing@gmail.com>
2014-10-02 04:13:29 +0800

    合完成

--- AxisWithPoints.cs
+++ AxisWithPoints.cs
@@ -57,6 +57,42 @@ namespace FindingClosestPoints
             double[][] nearPoints2 = FindClosest(PointsList.Skip(halfLength) as List<double[]>);
 
             //合
+            double neard1 = distance2Points(nearPoints1[0], nearPoints1[1]);
+            double neard2 = distance2Points(nearPoints2[0], nearPoints2[1]);
+            double neard;
+            if (neard1 <= neard2)
+            {
+                neard = neard1;
+                nearPoints = nearPoints1;
+            }
+            else
+            {
+                neard = neard2;
+                nearPoints = nearPoints2;
+            }
+            double halfX=PointsList[halfLength][0];
+            List<double[]> HalfLinePoints = new List<double[]>();
+            foreach (double[] point in PointsList)
+            {
+                if (Math.Abs(point[0] - halfX) < neard) HalfLinePoints.Add(point);
+            }
+            HalfLinePoints.Sort(SortByY);
+            for (int i = 0; i < HalfLinePoints.Count; i++)
+            {
+                for (int j = 1; j < 8; j++)
+                {
+                    double tempD = distance2Points(HalfLinePoints[i], HalfLinePoints[i + j]);
+                    if (tempD < neard)
+                    {
+                        neard = tempD;
+                        nearPoints[0] = HalfLinePoints[i];
+                        nearPoints[1] = HalfLinePoints[i + j];
+                    }
+                }
+            }
+            return nearPoints;
+
+
 
 
              
@@ -69,7 +105,7 @@ namespace FindingClosestPoints
 
         }
 
-        double distance2Points(double[] point1,double[] point2)
+        double distance2Points(double[] point1, double[] point2)
         {
             return Math.Sqrt(Math.Pow(point1[0]-point2[0],2)+Math.Pow(point1[1]-point2[1],2));
         }
@@ -77,6 +113,11 @@ namespace FindingClosestPoints
         static int SortByX(double[] point1, double[] point2)
         {
             return point1[0].CompareTo(point2[0]);
+        }
+
+        static int SortByY(double[] point1, double[] point2)
+        {
+            return point1[1].CompareTo(point2[1]);
         }
 
     }
